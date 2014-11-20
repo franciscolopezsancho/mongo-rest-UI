b7b2500917672a7f6143236edc391cf13b4f13d5
Francisco <franciscolopezsancho@gmail.com>
2014-11-20 15:29:02 +0000

    added most of the fields and saving but with some problems with the fields after parse all to a one level contact model

--- assets/js/apps/contacts/edit/edit_view.js
+++ assets/js/apps/contacts/edit/edit_view.js
@@ -4,14 +4,54 @@ ContactManager.module('ContactsApp.Edit', function(Edit,
 		template: "#contact-form",
 
 		events: {
-			'click button.js-submit': 'submitClicked'
+			'click button.js-submit': 'submitClicked',
+			'click input.js-supervised':'supervises',
+			'click input.js-matched':'matches'
 		},
-
 		submitClicked: function(e){
 			e.preventDefault();
 			console.log("edit contact");
+			console.log($("#link-supervised").prop('checked'))
+			console.log($("#link-matches").prop('checked'))
 			var data = Backbone.Syphon.serialize(this);
 			this.trigger("form:submit", data);
+		},
+		supervises: function(e){
+			e.preventDefault();	
+			if($("#link-supervised").attr("checked") == "checked"){
+				$("#link-supervised").removeAttr("checked");
+				this.model.set("supervised",false)
+			}else{
+				$("#link-supervised").prop("checked","checked");
+				this.model.set("supervised",true)
+			}
+			this.render();
+			console.log($("#link-supervised").attr("checked"));
+
+			
+		},
+		matches: function(e){
+			e.preventDefault();	
+			if($("#link-matched").attr("checked") == "checked"){
+				$("#link-matched").removeAttr("checked");
+				this.model.set("matched",false)
+			}else{
+				$("#link-matched").prop("checked","checked");
+				this.model.set("matched",true)
+			}
+			this.render();
+			console.log($("#link-matched").attr("checked"));			
+		},
+
+		check: function(el){
+			if($(el).attr("checked") == "checked"){
+				$("#link-matches").removeAttr("checked");
+				this.model.set("matches",false)
+			}else{
+				$(el).prop("checked","checked");
+				this.model.set("matches",true)
+			}
+			this.render();
 		}
 	});
 });
--- assets/js/entities/contact.js
+++ assets/js/entities/contact.js
@@ -3,11 +3,35 @@ ContactManager.module("Entities", function(Entities, ContactManager, Backbone, M
   parse: function (response) {
     if (response._id) {
       response.id = response._id
+      response.content=response.interaction.interaction.content
+      response.link=response.interaction.interaction.link
+      response.tags=response.interaction.interaction.tags
+      response.type=response.interaction.interaction.type
+      response.normalized_url=response.interaction.links.normalized_url
+      response.url=response.interaction.links.url
+      response.title=response.interaction.links.title
+      response.status=response.ifpi.process.status
+      response.detail=response.ifpi.process.detail
+      response.date=response.ifpi.process.date
+      response.matched=response.ifpi.process.match
+      response.track=response.ifpi.process.track
+      response.artist=response.ifpi.process.artist
+      response.supervised=response.ifpi.process.supervised  
+
     }
+    //response.unset("interaction")
+    //response.unset("ifpi")
     return response;
-},
+  },
+  sync: function(method, model, options) {
+    if (model.bundle) {
+      _.defaults(options || (options = {}), {attrs: model.bundle()});
+    }
+    return Backbone.sync.apply(this, [method, model, options]);
+  },
+
    // url: "http://54.77.180.70:3000/ml/supervisingUI",
-    url : function() {
+   url : function() {
      //Important! It's got to know where to send its REST calls. 
      //In this case, POST to '/donuts' and PUT to '/donuts/:id'
      return this.id ? "http://54.77.180.70:3000/ml/supervisingUI/" + this.id : "http://54.77.180.70:3000/ml/supervisingUI"; 
@@ -16,14 +40,14 @@ ContactManager.module("Entities", function(Entities, ContactManager, Backbone, M
 
 
 
- Entities.ContactCollection = Backbone.Collection.extend({
+Entities.ContactCollection = Backbone.Collection.extend({
   model: Entities.Contact,
-  url: "http://54.77.180.70:3000/ml/supervisingUI?limit=2"
+  url: "http://54.77.180.70:3000/ml/supervisingUI?limit=100"
 });
 
- var contacts;
+var contacts;
 
- var initializeContacts = function(){
+var initializeContacts = function(){
   contacts = new Entities.ContactCollection();
   contacts.fetch({
 
